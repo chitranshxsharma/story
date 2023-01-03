bb80d8391c6e5ed05abeee7e4d4859589227adf9
chitranshxsharma <113257610+chitranshxsharma@users.noreply.github.com>
2023-01-03 19:18:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var arr =[
+    {Dp:"https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",story:"https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"},
+    {Dp:"https://images.unsplash.com/photo-1614204424926-196a80bf0be8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",story:"https://images.unsplash.com/photo-1514626585111-9aa86183ac98?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"},
+    {Dp:"https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80" , story:"https://images.unsplash.com/photo-1557053910-d9eadeed1c58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"},
+    {Dp:"https://images.unsplash.com/photo-1581403341630-a6e0b9d2d257?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80", story:"https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1888&q=80"},
+]
+
+var clutter ="";
+arr.forEach(function(val, idx){
+    clutter = clutter + `<div class="story">
+        <div id="inner">
+            <img id="${idx}"src="${val.Dp}" alt="">
+        </div>
+    </div>`
+
+});
+var prog=0;
+var timer;
+var percent=0;
+document.querySelector("#stories").innerHTML = clutter;
+
+document.querySelector("#stories").addEventListener("click", function(dets){
+   document.querySelector("#fs img").src = arr[dets.target.id].story;
+   document.querySelector("#fs").style.display="flex";
+  
+   timer = setInterval(function(){
+    if(percent===100){
+      document.querySelector("#progmaker").style.width ="100%";
+      percent=0;
+      clearInterval(timer);
+    }
+    percent++;
+    document.querySelector("#progmaker").style.width=`${percent}%`;
+   },30)
+
+  setTimeout(function(){
+    document.querySelector("#fs").style.display = "none";
+
+   },3000)
+});
+
